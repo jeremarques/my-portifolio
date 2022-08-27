ad816afb14c80b97d7d3bd249644ca81ba37c415
Jeremias Marques <saimerejmarques@gmail.com>
2022-08-27 11:14:03 -0300

    update projects section

--- index.js
+++ index.js
@@ -14,79 +14,44 @@ let tl = gsap.timeline()
   .from('.anim-nav', {
     duration: 2,
     stagger: {each: 0.2},
-    y: 300,
+    y: 200,
     opacity: 0,
-    ease: "back.out(1)",
+    ease: "power4.out",
     delay: 3.8,
   })
   .from('.anim-info', {
     duration: 2,
-    stagger: {each: 0.2},
-    y: 200,
+    stagger: {each: 0.1},
+    x: -200,
     opacity: 0,
     ease: "back.out(1.7)",
   }, '-=2.2')
   .from('#logomarca-home', {
     duration: 2,
-    y: 200,
+    y: 100,
     opacity: 0,
-    ease: "back.out(1.4)",
-  }, '-=2.1')
+    ease: "power4.out",
+  }, '-=2.2')
   .from('#avatar', {
     duration: 2,
-    y: 200,
+    x: 100,
     opacity: 0,
-    ease: "back.out(1.4)",
+    ease: "back.out",
   }, '-=2.2')
   .from('.anim-tech', {
     duration: 2,
     y: 100,
     opacity: 0,
     stagger: {each: 0.2},
-    ease: "back.out(1.4)",
+    ease: "power4.out",
   }, '-=2.0')
-  
 
 /* Function that load the sreen load */
-
+const body = document.querySelector('body')
 function preLoading() {
   preloader.style.display = "none"
-  content.style.display = "block"
+  body.style.overflow = 'auto'
   navigationBar.style.display = "flex"
 }
 
 setTimeout(preLoading, 3800);
-
-/* Function whitch set this greeting in home section */
-
-function setGreeting() {
-  const date = new Date()
-  const hour = date.getHours()
-
-  if (hour >= 2 && hour < 12) {
-    greetingMessage.innerHTML = "BOM DIA! EU SOU"
-  } else if (hour >= 12 && hour < 19) {
-    greetingMessage.innerHTML = "BOA TARDE! EU SOU"
-  } else {
-    greetingMessage.innerHTML = "BOA NOITE! EU SOU"
-  }
-}
-
-setGreeting()
-
-/* Slider scritps */
-
-btnPrev.addEventListener('click', moveInRightDirectionSlider)
-btnNext.addEventListener('click', moveInLeftDirectionSlider)
-const shadow = document.getElementById('shadow')
-
-function moveInLeftDirectionSlider() {
-  slider.style.marginLeft = '-500px'
-  setTimeout(() => {
-    shadow.style.opacity = 0
-  }, 700);
-}
-function moveInRightDirectionSlider() {
-  slider.style.marginLeft = '0'
-  shadow.style.opacity = 100
-}
